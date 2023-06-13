2c754a3f316f600f7b1c5d177551794cea1f14db
Jiang Yan Ting <B082126@stumail.ltu.edu.tw>
2023-06-13 22:54:58 +0800

    增加控制器

---
+++ img/JS.js
@@ -0,0 +1,6 @@
+const toggleButton = document.querySelector('#toggle-button');
+const toggle = document.querySelector('#toggle');
+const toggleBall = document.querySelector('.toggle-ball');
+const toggleContainer = document.querySelector('.toggle-container');
+const circleLabel = document.querySelector('#circle');
+const squareLabel = document.querySelector('#square');
